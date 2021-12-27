1b74eb9a0c5954192856b0964c67bb73ac6657f5
WuphonsReach <wuphonsreach@gmail.com>
2021-12-27 13:42:55 -0500

    add tests for WhenCombo name/CP values

--- dotnet/test/Data.Tests/CraftingActionsTests.cs
+++ dotnet/test/Data.Tests/CraftingActionsTests.cs
@@ -8,12 +8,16 @@ namespace Data.Tests
         private readonly CraftingActions _sut = new CraftingActions();
         private readonly MaterialConditions _materialConditions = new MaterialConditions();
 
+        private const int MaximumCraftingPoints = 999;
+
         [Fact]
         public void All_is_not_empty()
         {
             Assert.NotEmpty(_sut.All.Value);
         }
 
+        //TODO: Consider adding DataAnnotation or FluentValidation
+        
         [Fact]
         public void Name_is_provided()
         {
@@ -42,10 +46,10 @@ namespace Data.Tests
         }
 
         [Fact]
-        public void CP_is_value_0_to_999()
+        public void CP_is_value_0_to_MaximumCraftingPoints()
         {
             var result = _sut.All.Value
-                .Where(x => !(x.Value.CP >= 0 && x.Value.CP <= 999))
+                .Where(x => !(x.Value.CP >= 0 && x.Value.CP <= MaximumCraftingPoints))
                 .ToList();
             Assert.Empty(result);
         }
@@ -68,6 +72,48 @@ namespace Data.Tests
                         _materialConditions.All.Value.Keys.Contains(value), 
                         $"Action ({action.Key}): Value '{value}' not valid in {nameof(action.Value.WhenMaterialCondition)} list."
                         );
+                }
+            }
+        }
+        
+        [Fact]
+        public void WhenCombo_Action_is_valid()
+        {
+            var actions = _sut.All.Value
+                .Where(x => x.Value
+                .WhenCombo?.Any() == true).ToList();
+
+            foreach (var action in actions)
+            {
+                var values = action.Value.WhenCombo.Select(y => y.Action).ToList();
+                foreach (var value in values)
+                {
+                    Assert.True(
+                        _sut.All.Value.Keys.Contains(value), 
+                        $"Action ({action.Key}): Value '{value}' not valid {nameof(WhenComboAction.Action)} in {nameof(action.Value.WhenCombo)} list."
+                    );
+                }
+            }
+        }
+        
+        [Fact]
+        public void WhenCombo_CP_is_valid()
+        {
+            var actions = _sut.All.Value
+                .Where(x => x.Value
+                    .WhenCombo?.Any() == true).ToList();
+
+            foreach (var action in actions)
+            {
+                var values = action.Value.WhenCombo
+                    .Where(x => x.CP.HasValue)
+                    .ToList();
+                foreach (var value in values)
+                {
+                    Assert.True(
+                        value.CP >= 0 && value.CP <= MaximumCraftingPoints, 
+                        $"Action ({action.Key}): Value '{value.CP}' not valid {nameof(WhenComboAction.CP)} in {nameof(action.Value.WhenCombo)} list."
+                    );
                 }
             }
         }

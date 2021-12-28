03a85fe86b8b946141881e51d08322ab40935084
WuphonsReach <wuphonsreach@gmail.com>
2021-12-28 07:17:30 -0500

    add properties to item/recipe classes

--- dotnet/src/Solver/Data/Teamcraft/TeamcraftItem.cs
+++ dotnet/src/Solver/Data/Teamcraft/TeamcraftItem.cs
@@ -1,7 +1,26 @@
+using Newtonsoft.Json;
+
 namespace WuphonsReach.FF14Crafting.Solver.Data.Teamcraft
 {
     public class TeamcraftItem
     {
+        [JsonProperty("en")]
+        public string English { get; set; }
+        [JsonProperty("de")]
+        public string German { get; set; }
+        [JsonProperty("ja")]
+        public string Japanese { get; set; }
+        [JsonProperty("fr")]
+        public string French { get; set; }
         
+        public string Name()
+        {
+            // later, this method will take a locale argument
+
+            // Not all item IDs have a name defined (unused item IDs)
+            return string.IsNullOrWhiteSpace(English)
+                ? "(no name)"
+                : English;
+        }
     }
 }
--- dotnet/src/Solver/Data/Teamcraft/TeamcraftRecipe.cs
+++ dotnet/src/Solver/Data/Teamcraft/TeamcraftRecipe.cs
@@ -1,7 +1,24 @@
+using Newtonsoft.Json;
+
 namespace WuphonsReach.FF14Crafting.Solver.Data.Teamcraft
 {
     public class TeamcraftRecipe
     {
+        public int Id { get; set; }
+
+        [JsonProperty("job")]
+        public int JobId { get; set; }
+
+        [JsonProperty("lvl")]
+        public int Level { get; set; }
+        
+        public int Yields { get; set; }
+        
+        /// <summary>Item ID of the result.  See <see cref="TeamcraftItems"/> dictionary
+        /// keys to obtain the item name from the <see cref="TeamcraftItem"/>.</summary>
+        [JsonProperty("result")]
+        public int ResultId { get; set; }
+ 
         
     }
 }
